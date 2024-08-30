d934629ff50d1acb1e08eb30ed97048936d468e9
dylanstead <dylanstead@gmail.com>
2024-08-30 15:25:58 +0100

    About Page

--- devadopts-project/src/App.jsx
+++ devadopts-project/src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-import { RegisterPage, NotFoundPage, LoginPage} from './pages';
-import {Route, Routes} from 'react-router-dom';
+import "./App.css";
+import { RegisterPage, NotFoundPage, LoginPage, AboutPage } from "./pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RegisterPage />}/>
-      <Route path="/Login" element={<LoginPage />}/>
-      <Route path="*" element={<NotFoundPage />}/>
+      <Route path="/" element={<RegisterPage />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="/About" element={<AboutPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ devadopts-project/src/pages/AboutPage/index.jsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        "Since 1891, we’ve been working for a better future for dogs and the
+        people who love them. When a dog is in distress, we care for them. When
+        a dog needs a home, we find them a loving family. When an owner needs a
+        helping hand (or paw) – or they just can’t cope, we’re ready to step in.
+        For every dog, for every owner, we’re by their side through thick and
+        thin, throughout their lives. The bond they have is special to us. It
+        changes lives, making each day happier and more complete. It’s why we
+        believe A dog is for life. Discover who we are, what we do, and how
+        we’ll never stop fighting to make tomorrow’s world a better place for
+        all dogs."
+      </p>
+    </div>
+  );
+}

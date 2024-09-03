b4acfd041df20182e1f0818231c6a3d80f0e01e9
dylanstead <dylanstead@gmail.com>
2024-09-03 12:29:04 +0100

    Change the navigation bar

--- devadopts-project/src/components/PageWrapper/index.jsx
+++ devadopts-project/src/components/PageWrapper/index.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import './PageWrapper.css';
-import { NavLink, Outlet } from 'react-router-dom';
+import React from "react";
+import "./PageWrapper.css";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function PageWrapper() {
   return (
     <>
-        <header className='navbar-container navbar'>
-            <nav>
-                <NavLink to="/login">Login</NavLink>
-                <NavLink to="/register">Register</NavLink>
-                <NavLink to="/donate">Donate</NavLink>
-                <NavLink to="/AboutUs">About Us</NavLink>
-            </nav>
-        </header>
-        <Outlet />
-        <footer className='footer'>
-            <nav>
-                <p>Copyright 2024</p>
-            </nav>
-        </footer>
+      <header className="navbar-container">
+        <div className="navbar">
+          <div className="brand">
+            <img
+              src="your-icon-path.png"
+              alt="DevAdopts Logo"
+              className="brand-icon"
+            />
+            <h1>DevAdopts</h1>
+          </div>
+          <nav className="nav-links">
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/donate">Donate</NavLink>
+            <NavLink to="/AboutUs">About Us</NavLink>
+          </nav>
+        </div>
+      </header>
+      <Outlet />
+      <footer className="footer">
+        <nav>
+          <p>Copyright 2024</p>
+        </nav>
+      </footer>
     </>
-  )
+  );
 }

ac9fdcfdd6d921c548ce06e974599f2d3085b152
dylanstead <dylanstead@gmail.com>
2024-09-04 09:48:30 +0100

    Change userprofile

--- devadopts-project/src/pages/UserProfilePage/index.jsx
+++ devadopts-project/src/pages/UserProfilePage/index.jsx
@@ -1,60 +1,61 @@
-// import React, { useEffect, useState } from "react";
-// import { jwtDecode } from "jwt-decode";
-// import axios from "axios";
-// import "./userprofilepage.css";
+import React, { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import "./userprofilepage.css";
 
-// export default function UserProfilePage() {
-//   const [userDetails, setUserDetails] = useState(null);
-//   const token = localStorage.getItem("token");
-//   const decodedtoken = jwtDecode(token);
-//   const userId = decodedtoken.userId;
+export default function UserProfilePage() {
+  const [userDetails, setUserDetails] = useState(null);
+  const token = localStorage.getItem("token");
+  console.log("token", token);
+  const decodedtoken = jwtDecode(token);
+  const userId = decodedtoken.userId;
 
-//   useEffect(() => {
-//     const getUser = async () => {
-//       try {
-//         const response = await axios.get(
-//           `http://localhost:3000/users/${userId}`,
-//           {
-//             headers: {
-//               authorization: `Bearer ${token}`,
-//             },
-//           }
-//         );
-//         setUserDetails(response.data.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     getUser();
-//   }, [token, userId]);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/users/${userId}`,
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setUserDetails(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUser();
+  }, [token, userId]);
 
-//   console.log("userDeatils", userDetails);
+  console.log("userDeatils", userDetails);
 
-//   if (userDetails) {
-//     return (
-//       <div className="container">
-//         <div className="sidebar-container">
-//           <div className="button">
-//             <p className="text">Account Information</p>
-//           </div>
-//           <div className="button">
-//             <p className="text">Preferences</p>
-//           </div>
-//           <div className="button">
-//             <p className="text">Favourites</p>
-//           </div>
-//           <div className="button">
-//             <p className="text">Donations</p>
-//           </div>
-//         </div>
-//         <div className="information-container">
-//           <p className="text1">First Name: {userDetails.first_name}</p>
-//           <p className="text1">Surname: {userDetails.last_name}</p>
-//           <p className="text1">Email: {userDetails.email}</p>
-//         </div>
-//       </div>
-//     );
-//   } else {
-//     return <div>Loading</div>;
-//   }
-// }
+  if (userDetails) {
+    return (
+      <div className="container">
+        <div className="sidebar-container">
+          <div className="button">
+            <p className="text">Account Information</p>
+          </div>
+          <div className="button">
+            <p className="text">Preferences</p>
+          </div>
+          <div className="button">
+            <p className="text">Favourites</p>
+          </div>
+          <div className="button">
+            <p className="text">Donations</p>
+          </div>
+        </div>
+        <div className="information-container">
+          <p className="text1">First Name: {userDetails.first_name}</p>
+          <p className="text1">Surname: {userDetails.last_name}</p>
+          <p className="text1">Email: {userDetails.email}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Loading</div>;
+  }
+}

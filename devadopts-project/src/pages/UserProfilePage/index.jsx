4d9fd01884605d0d9191499fb732d0a2179fdbb3
reebujoy <reebsmj@gmail.com>
2024-09-04 11:31:25 +0100

    Update UserProfile Page

--- devadopts-project/src/pages/UserProfilePage/index.jsx
+++ devadopts-project/src/pages/UserProfilePage/index.jsx
@@ -1,21 +1,22 @@
 import React, {useState, useEffect} from 'react';
-import axios from "axios";
 import {FavoriteDogs} from '../../components';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-import './UserProfile.css';
+import './UserProfile.css'
+import axios from 'axios';
 
 export default function UserProfilePage() {
   const [showFavoriteDogs, setShowFavoriteDogs] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [userDetails, setUserDetails] = useState({});
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token")
   const userId = localStorage.getItem("userId");
+  console.log(userId);
 
   useEffect(() => {
     const getUser = async () => {
       try {
+        setLoading(true);  // Start loading
         const response = await axios.get(
           `http://localhost:3000/users/${userId}`,
           {
@@ -26,12 +27,14 @@ export default function UserProfilePage() {
         );
         setUserDetails(response.data);
       } catch (error) {
-        console.error(error);
+        console.error('Error fetching user details:', error);
+      } finally {
+        setLoading(false);  // Stop loading
       }
     };
     getUser();
   }, [token, userId]);
-
+console.log(userDetails);
   return (
     <Tabs
     defaultActiveKey="profile"
@@ -39,16 +42,18 @@ export default function UserProfilePage() {
     className="mb-3"
     fill
     >
-      <Tab eventKey="home" title="Account Information">
-        {userDetails.data ? (
+      <Tab eventKey="profile" title="Account Information">
+        {loading ? (  // Show loading state
+          <p>Loading user details...</p>
+        ) : userDetails && userDetails.data ? (  // Render user details if available
           <>
             <p>First Name: {userDetails.data.first_name}</p>
             <p>Surname: {userDetails.data.last_name}</p>
             <p>Email: {userDetails.data.email}</p>
           </>
         ) : (
-          <p>Loading user details...</p>
-        )}  
+          <p>User details not found.</p>  // Show error if userDetails is null or undefined
+        )}
       </Tab>
       <Tab eventKey="favorites" title="Favorites">
         <FavoriteDogs />
